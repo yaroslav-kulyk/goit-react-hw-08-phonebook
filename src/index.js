93a6dfc8a7c154b1549abcd4df32c7be92658f54
yaroslav-kulyk <kulyk.yaroslav@gmail.com>
2021-11-26 16:02:56 +0200

    init persist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 import store from './redux/store';
 import App from './App';
 import './index.css';
@@ -9,9 +11,11 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,51 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import filterReducer from './phonebook/phonebook-reducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsApi } from './contactsSlice';
 import { authAPI } from './authSlice';
 import { authReducer } from './auth';
 import phonebookReducer from './phonebook/phonebook-slice';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistReducer(authPersistConfig, authReducer),
+//     todos: todosReducer,
+//   },
+//   middleware,
+//   devTools: process.env.NODE_ENV === 'development',
+// });
+
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    // auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
     phonebook: phonebookReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
     [authAPI.reducerPath]: authAPI.reducer,
   },
 
   middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
     contactsApi.middleware,
     authAPI.middleware,
   ],
@@ -23,3 +54,4 @@ const store = configureStore({
 });
 
 export default store;
+export const persistor = persistStore(store);
